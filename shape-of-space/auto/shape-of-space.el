f2a40e124036d3aba70ba053675ebcb1269a1c20
antonio teran <teran@mit.edu>
2019-05-30 17:34:49 -0700

    init shape of space

---
+++ shape-of-space/auto/shape-of-space.el
@@ -0,0 +1,31 @@
+(TeX-add-style-hook
+ "shape-of-space"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "10pt" "landscape")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "landscape")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "multicol"
+    "calc"
+    "ifthen"
+    "geometry"
+    "hyperref"
+    "amsmath"
+    "esdiff"
+    "cancel"
+    "mathtools")
+   (TeX-add-symbols
+    "BibTeX"))
+ :latex)
+
